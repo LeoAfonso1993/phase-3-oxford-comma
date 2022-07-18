24da8b8382a72392a10165d768f76a008195867d
Leandro Dos Santos Afonso <leandro.afonso@hotmail.com>
2022-07-17 23:01:47 -0400

    Completed Assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    oxford_str = ""
+    if array.length == 1
+        oxford_str << array[0]
+    elsif array.length == 2
+        oxford_str << array.join(" and ")
+    else
+        oxford_str << array[0..-2].join(", ")
+        oxford_str << ", and #{array[-1]}"
+    end
+    return oxford_str    
 end
